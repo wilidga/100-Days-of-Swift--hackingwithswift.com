7d770807c278f0fdd512fa34d78190c0d077ffb2
Wilman Garcia De Leon <wilidga@gmail.com>
2020-11-07 23:50:12 -0600

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Wilman Garcia De Leon on 11/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
